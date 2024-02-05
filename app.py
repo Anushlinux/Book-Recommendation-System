ca5178a0424e99dec5ae29282e63b67c43c74e32
Anushlinux <anushrutpandit@gmail.com>
2024-02-05 20:58:23 +0530

    recoomended books joined

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template ,request
 import pickle
+import numpy as np
 
 best_df = pickle.load(open('best_df.pkl', 'rb'))
+pt = pickle.load(open('pt.pkl', 'rb'))
+books = pickle.load(open('books.pkl', 'rb'))
+similarity_score = pickle.load(open('similarity_score.pkl', 'rb'))
 
 app = Flask(__name__)
 
@@ -19,6 +23,29 @@ def index():
 @app.route("/Recommend")
 def recommend_ui():
     return render_template("recommend.html")
+
+@app.route("/recommend_books", methods = ['POST'])
+def recommend():
+    user_input = request.form.get("user_input")
+    
+    index = np.where(pt.index == user_input)[0][0]
+    similar_items = sorted(list(enumerate(similarity_score[index])), key = lambda x:x[1], reverse=True)[1:9]
+    
+    data = []
+    for i in similar_items:
+        
+        item = []
+        temp_df = books[books["Book-Title"] == pt.index[i[0]]]
+        item.extend(list(temp_df.drop_duplicates("Book-Title")["Book-Title"].values))
+        item.extend(list(temp_df.drop_duplicates("Book-Title")["Book-Author"].values))
+        item.extend(list(temp_df.drop_duplicates("Book-Title")["Image-URL-S"].values))
+        
+        data.append(item)
+    
+    print(data)    
+        
+   
+    return render_template('recommend.html', data = data)
 
 if __name__ == '__main__':
     app.run(debug = True)
